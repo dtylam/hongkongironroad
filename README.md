79f2ee753a30c195359d3c343d746b83a811da01
Donald Lam <8455943+dtylam@users.noreply.github.com>
2025-02-15 20:39:30 +0000

    dynamic load east rail line

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hongkongironroad
+
+Hong Kong railway stations. But in literal translations.
+
+I hate having to update npm/js libraries, so wrote this with vanilla js 😝
+
+With a bit of help from [Shoelace](https://shoelace.style/)!
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,42 @@
-// This file is intentionally left blank.
+function populateCarouselItems(data) {
+  const carousel = document.querySelector(".stations-carousel");
+  const templateUrl = "components/carousel-item-template.html";
+  fetch(templateUrl)
+    .then((response) => response.text())
+    .then((templateHtml) => {
+      // Create a temporary element to hold the template
+      const templateElement = document.createElement("div");
+      templateElement.innerHTML = templateHtml;
+
+      data.forEach((item) => {
+        // Clone the template
+        const carouselItem = templateElement.firstElementChild.cloneNode(true);
+
+        // Populate fields
+        carouselItem.querySelector(".zh-name").textContent = item.zhName;
+        carouselItem.querySelector(".en-transliteration").textContent =
+          item.enTransliteration;
+
+        // Append the populated template to the carousel
+        carousel.appendChild(carouselItem);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching template:", error);
+    });
+}
+
+// ready function
+document.addEventListener("DOMContentLoaded", function () {
+  var filename = "data/EAL.json";
+  fetch(filename)
+    .then((response) => response.json())
+    .then((data) => {
+      //   console.debug(data);
+      populateCarouselItems(data);
+    })
+    .catch((error) => {
+      // Your code to handle any errors
+      console.debug("Error fetching JSON:", error);
+    });
+});
